52d0a9be9bece0a63c9b2dd4e29fec47cce921e9
shafkathullah ihsan <shafkathullah28@gmail.com>
2017-02-14 11:58:59 +0400

    Sketchr Initial completion

--- work/Sketchr/main.js
+++ work/Sketchr/main.js
@@ -1,27 +1,60 @@
 $(document).ready(function(){
-    var winW = $(window).width();
-    var winH = $(window).height();
-    var areaT = winW * winH;
-    var numDivs = areaT / 260;
-    var eraser = false;
-    var wrap = $(".wrapper");
-    for(var i=0;i<numDivs;i++){
-        wrap.append("<div class='block'></div>");
+  var widthWrapper = $(".wrapper").width();
+  var widthBlock = widthWrapper/100;
+  var num = widthWrapper/widthBlock;
+  var mul = num/2 * num;
+  var wrap = $(".wrapper");
+  
+
+    for(i=0;i<mul;i++){
+      wrap.append("<div class='block'></div>");
     }
 
-     var paint = 1;
+  $(".block").width(widthBlock).height(widthBlock);
+  $(".toolbox").height($(".wrapper").height());
 
-     $(".block").mousedown(function(){
-        
-     });
-        
-    
-    
-    
-    
+  $("#colr").on("change",function(){
+    $(".color").css("background-color",$("#colr").val());
+  });
+
+  $( "input" ).on( "click", function() {
+    if($( "input:checked" ).val() == 'pen'){
+
+      function paint(){
+        $(this).css('background-color',$("#colr").val());
+      }
+
+      $(".block").on('mousedown',function(event){
+        event.preventDefault();
+        $(this).css('background-color',$("#colr").val());
+        $(".block").on('mouseenter',paint);
+      });
+
+      $(".block").on('mouseup',function(event){
+        event.preventDefault();
+        $(".block").off('mouseenter',paint);
+      });
+    }
+    else{
+      function erase(){
+        $(this).css('background-color','white');
+      }
+
+      $(".block").on('mousedown',function(event){
+        event.preventDefault();
+        $(this).css('background-color','white');
+        $(".block").on('mouseenter',erase);
+      });
+
+      $(".block").on('mouseup',function(event){
+        event.preventDefault();
+        $(".block").off('mouseenter',erase);
+      });
+    }
+  });
+
+  $(".clear").on("click",function(){
+    $(".block").css("background-color","white");
+  });
 
 });
-
-
-
-
